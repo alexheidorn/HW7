8304b85f6db4bb06d0e4096654c1f95c3ce4afa4
alexheidorn <alexheidorn12@gmail.com>
2023-10-17 14:50:45 -0500

    not working ;(

--- hw7.cpp
+++ hw7.cpp
@@ -15,28 +15,50 @@ using namespace std;
 class Queue{
 private:
     vector<string> data;
-    int first;
-    int next;
+    int currentFront = 0;
+    int nextAvail = -1;
 public:
     Queue(){
         data.resize(4);
-        first = 0;
-        next = -1;
     }
     int size(){ return data.size(); }
     bool empty() { return data.empty(); }
     void push(string v) { 
-        if (first == next){
-            cerr 
+        if (currentFront == nextAvail){ //if queue is full
+            cerr << "Queue is empty";
+        }
+        else if (data.empty()){
+            data[0] = v;
+            currentFront = 0;
+            nextAvail = 1;
+        }
+        else{
+            data[nextAvail] = v;
+            nextAvail = (nextAvail + 1) % data.size();
         }
     }
-    void pop()
-    void front() { }
+    void pop(){
+        if (data.empty()){ // next == -1
+            cout << "Queue is empty";
+        }
+        else{
+            currentFront = (currentFront + 1) % data.size();
+            if (currentFront == nextAvail){ //if because empty
+                currentFront = 0;
+                nextAvail = -1;
+            }
+        }
+        
+    }
+    string front() { return data[currentFront]; }
 };
 
 int main()
 {
-    cout << "Hello World!\n";
-
-    return 0;
+    Queue myQueue;
+    myQueue.push("hi");
+    myQueue.push("bruh");
+    myQueue.front();
+    myQueue.pop();
+    myQueue.front();
 }
