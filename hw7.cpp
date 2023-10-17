a435c32b9996e904a98c2114713f9eb09d5e55c0
alexheidorn <alexheidorn12@gmail.com>
2023-10-17 14:24:13 -0500

    starting to build class fxns

--- hw7.cpp
+++ hw7.cpp
@@ -1,5 +1,6 @@
 /*
 * Alex Heidorn
+* Teammates: Aidan Carlson, Anna Wolf
 * Created: 10-17-23
 * CS2 HW7
 *  
@@ -10,6 +11,28 @@
 #include <string>
 
 using namespace std;
+
+class Queue{
+private:
+    vector<string> data;
+    int first;
+    int next;
+public:
+    Queue(){
+        data.resize(4);
+        first = 0;
+        next = -1;
+    }
+    int size(){ return data.size(); }
+    bool empty() { return data.empty(); }
+    void push(string v) { 
+        if (first == next){
+            cerr 
+        }
+    }
+    void pop()
+    void front() { }
+};
 
 int main()
 {
