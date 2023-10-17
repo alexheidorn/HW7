531ce6dd8d92d8c4bf063346003b27150fce2cec
alexheidorn <alexheidorn12@gmail.com>
2023-10-17 14:06:56 -0500

    first commit

---
+++ hw7.cpp
@@ -0,0 +1,19 @@
+/*
+* Alex Heidorn
+* Created: 10-17-23
+* CS2 HW7
+*  
+*/
+
+#include <iostream> 
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
